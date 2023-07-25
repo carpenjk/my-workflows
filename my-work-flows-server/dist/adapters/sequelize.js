ffacb6b8771e4f2cd9a2acf9b792d2e037cb3c11
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-07-25 14:30:46 -0400

    rename server directory

--- my-work-flows-server/dist/adapters/sequelize.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = void 0;
-const sequelize_1 = require("sequelize");
-require('dotenv').config();
-exports.sequelize = new sequelize_1.Sequelize(process.env.database, process.env.SQL_USER, process.env.SQL_PASSWORD, {
-    host: process.env.SQL_HOST,
-    dialect: 'mysql'
-});
--- my-work-flows-server/dist/middleware/notFoundHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = void 0;
-const notFoundError_1 = require("../errors/notFoundError");
-const notFoundHandler = (req, res, next) => {
-    return next(new notFoundError_1.NotFoundError('Route does not exist.'));
-};
-exports.notFoundHandler = notFoundHandler;
