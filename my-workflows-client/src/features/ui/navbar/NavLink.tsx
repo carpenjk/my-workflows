967762f64d72eaf9cc23c5b8e8271f5fbd112406
carpenjk <56424605+carpenjk@users.noreply.github.com>
2023-07-28 18:47:11 -0400

    theme fix

--- my-workflows-client/src/features/ui/navbar/NavLink.tsx
+++ my-workflows-client/src/features/ui/navbar/NavLink.tsx
@@ -11,7 +11,7 @@ const CustomNavLink = ({to, collapsed,children}: Props) => {
   return ( 
     <NavLink 
     className={({isActive}) =>  twMerge(` w-full flex items-center justify-center 
-      sm:justify-stretch  text-xs sm:text-sm dark:text-dk-text-normal 
+      sm:justify-stretch  text-xs sm:text-base dark:text-dk-text-normal 
       text-text-normal hover:bg-primary-9 dark:hover:bg-dk-primary-7 rounded-sm sm:rounded-md 
       overflow-x-hidden transition-all truncate duration-300 ease-in-out font-normal font-roboto`,
       isActive && " dark:bg-dk-primary-7 bg-primary-9 ", collapsed && "w-navbar-collapsed flex-none ")}
--- my-workflows-client/src/features/ui/navbar/Navbar.tsx
+++ my-workflows-client/src/features/ui/navbar/Navbar.tsx
@@ -21,14 +21,29 @@ const Navbar = ({className}: Props) => {
           , className)}>
         <nav className='relative w-full'>
           <ul className="flex justify-between w-full sm:space-y-6 sm:flex-col ">
-            <NavItem collapsed={isCollapsed} to="/" ><HomeIcon className='flex-none w-5 h-5 fill '/><NavText>Dashboard</NavText></NavItem>
-            <NavItem collapsed={isCollapsed} to="/workflow" ><SquaresPlusIcon className='flex-none w-5 h-5'/><span className='flex space-x-1.5'><NavText>Create</NavText><NavText className='hidden sm:flex'>Workflow</NavText></span></NavItem>
-            <NavItem collapsed={isCollapsed} to="/manage" ><TableCellsIcon className='flex-none w-5 h-5'/><span className='flex space-x-1.5'><NavText>Manage</NavText><NavText className='hidden sm:flex'> Workflows</NavText></span></NavItem>
+            <NavItem collapsed={isCollapsed} to="/" >
+              <HomeIcon className='flex-none w-5 h-5 fill '/><NavText>Dashboard</NavText></NavItem>
+            <NavItem collapsed={isCollapsed} to="/workflow" >
+              <SquaresPlusIcon className='flex-none w-5 h-5'/>
+              <span className='flex space-x-1.5'>
+                <NavText>Create</NavText><NavText className='hidden sm:flex'>Workflow</NavText>
+              </span>
+            </NavItem>
+            <NavItem collapsed={isCollapsed} to="/manage" >
+              <TableCellsIcon className='flex-none w-5 h-5'/>
+              <span className='flex space-x-1.5'>
+                <NavText>Manage</NavText><NavText className='hidden sm:flex'> Workflows</NavText>
+              </span>
+            </NavItem>
             {!user?.email && 
-              <NavItem collapsed={isCollapsed} to="/login" ><UserIcon className='flex-none w-5 h-5'/><NavText>Login</NavText></NavItem>
+              <NavItem collapsed={isCollapsed} to="/login" ><UserIcon className='flex-none w-5 h-5'/>
+                <NavText>Login</NavText>
+              </NavItem>
             }
             {user?.email && 
-            <NavItem collapsed={isCollapsed} to="/logout" ><UserIcon className='flex-none w-5 h-5'/><NavText>Logout</NavText></NavItem>
+            <NavItem collapsed={isCollapsed} to="/logout" >
+              <UserIcon className='flex-none w-5 h-5'/><NavText>Logout</NavText>
+            </NavItem>
             }
           </ul>
           <button onClick={()=> setIsCollapsed((prev)=> !prev)} className='absolute hidden p-1 border rounded-md sm:flex text-text-normal dark:text-dk-text-normal slate-400 w-7 h-7 -right-11 top-3 hover:animate-bounce-once border-primary-6 dark:border-dk-primary-6'><ArrowsRightLeftIcon/></button>
